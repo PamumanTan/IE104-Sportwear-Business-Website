acdf966e54a7af39bb8ae672ecf4a4a8d510bc9e
PamumanTan <96417133+PamumanTan@users.noreply.github.com>
2023-11-27 10:27:30 +0700

    Delete old files

--- components/cart-product-item/script.js
+++
@@ -1,7 +0,0 @@
-//JS Code để lấy dữ liệu product từ api
-const productImage = document.querySelector('.cart-product-img img');
-const productName = document.querySelector('.cart-product-detail #product-name');
-const productSize = document.querySelector('.cart-product-detail #product-size');
-const productQuantity = document.querySelector('.cart-product-detail #product-quantity');
-const productPrice = document.querySelector('.cart-product-detail #product-price');
-const deleteButton = document.querySelector('.cart-product-delete');
